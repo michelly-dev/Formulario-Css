6cd183099023e6ece64a756a597eefe2735a2a74
Michelly Ferreira <dev.michelly.f@gmail.com>
2021-04-19 13:23:23 -0300

    first commit

---
+++ aap.js
@@ -0,0 +1,31 @@
+console.log('Hello Cataline')
+var name = 'Michelly Silva'
+var age = 27
+var weight = 49.9
+var alive = true
+var food = null
+
+var person = {
+    name: 'Michelly Silva',
+    age: 27,
+    alive: true,
+    height: 49.9
+}
+
+console.log(typeof age)
+
+var product = 'Camisa'
+var price = 10
+
+console.log('O preço da ' + product + ' é R$ ' + price)
+
+var x = 10
+var y = 3
+
+var result = x + y
+
+console.log(result)
+
+
+
+
